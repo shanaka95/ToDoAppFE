9067982a678294b0c901bbbe0e85899bd9186e28
Shanaka Anuradha <shanaka95@gmail.com>
2023-01-25 11:13:01 +0530

    Initialize Angular Project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+
+    // TODO: Change this to production api URL
+    apiUrl: 'http://127.0.0.1:8000'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+
+    // Development server URL
+    apiUrl: 'http://127.0.0.1:8000'
+};
