adab254c12b3d105d956f381f4047f8c3195cc97
Shanaka Anuradha <shanaka95@gmail.com>
2023-01-26 12:50:33 +0530

    Added responsiveness to the login page

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,3 +6,10 @@
 
 -   Architecture developed with NgRx and Lazyloading [@DeborahK](https://github.com/DeborahK/Angular-NgRx-GettingStarted/tree/master/APM-Demo5).
 -   ESLint, Prettier and EditorConfig Added
+
+## [1.0.1] - 2023-01-26
+
+### Added
+
+-   Login Page Created from scratch. Design highly inspired by [@LOGIFY](<https://www.figma.com/file/AMVdaBTuWnaTyTufsrfHcu/LOGIFY---WEB-LOGIN-UI-KIT-(Community)-(Community)?node-id=0%3A1&t=av6KCVZi5iMrlKv6-0>).
+-   Added responsiveness to the login page
--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,5 @@
-import { Component , ViewEncapsulation } from '@angular/core';
-
+import { Component , OnInit, ViewEncapsulation } from '@angular/core';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +7,33 @@ import { Component , ViewEncapsulation } from '@angular/core';
   styleUrls: ['./login.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+    isMobile = true;
+
+    constructor(private responsive: BreakpointObserver) {
+
+    }
+
+    ngOnInit() {
+
+        this.responsive.observe([
+                Breakpoints.Web,
+                Breakpoints.WebLandscape,
+                Breakpoints.WebPortrait,
+                Breakpoints.Medium,
+                Breakpoints.Large,
+                Breakpoints.XLarge
+            ])
+            .subscribe(result => {
+
+                this.isMobile = true;
+
+                if (result.matches) {
+                    this.isMobile = false;
+                }
+
+            });
+
+    }
 }
